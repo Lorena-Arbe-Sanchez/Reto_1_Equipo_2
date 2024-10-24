e70a8634b5f3bbfeddc1862e6a8ab0b296f40801
aritz <aritz.saiz@ikasle.egibide.org>
2024-10-24 08:58:34 +0200

    cambios PHP

--- Aplicacion/php/mvc_reto/controller/UsuarioController.php
+++ Aplicacion/php/mvc_reto/controller/UsuarioController.php
@@ -19,15 +19,55 @@ class UsuarioController {
 
     }
 
+    //Crear Usuario
+
+    public function create() {
+
+        $this->page_title = "Crear Usuario";
+        $this->view = "crearCuenta";
+
+    }
+
+    public function save(){
+
+        $this->page_title = "Crear Usuario";
+        $this->view = "crearCuenta";
+
+        $param = $_POST;
+        $id = $this->model->save($param);
+        $result = $this->model->getBodegaById($id);
+
+        $_GET["responde"] = true;
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 
     //Funcion para al hacer login mirar si existe el usuario
     public function getUsuario($usuario, $contrasenya) {
 
         $this->page_title = "Usuario";
+        $this->view = "usuario";
         $param = $_POST;
         $result =$this->model->getBodegaByUsuarioContrasenya($usuario, $contrasenya);
         $_GET["responde"] = true;
         return $result;
+
+    }
+
+    public function UsuarioAdmin(){
+        $this->page_title = "Usuario";
+        $this->view = "usuario";
 
     }
 
--- Aplicacion/php/mvc_reto/model/Usuario.php
+++ Aplicacion/php/mvc_reto/model/Usuario.php
@@ -1,32 +1,35 @@
 <?php
 
-class Usuario {
+class Usuario
+{
 
     private $tabla = "usuarios";
     private $conection;
 
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->getConexion();
     }
 
-    public function getConexion(){
+    public function getConexion()
+    {
         $dbObj = new Db();
-        $this->conection = $dbObj ->conection;
+        $this->conection = $dbObj->conection;
     }
 
 
-    public function getBodegaByUsuarioContrasenya($usuario, $contrasenya){
+    public function getBodegaByUsuarioContrasenya($usuario, $contrasenya)
+    {
 
-        if(is_null($usuario, $contrasenya)) return false;
+        if (is_null($usuario, $contrasenya)) return false;
 
         $sql = "SELECT * FROM " . $this->tabla . " WHERE usuario = ?" . "AND contrasña = ?";
 
         $stmt = $this->conection->prepare($sql);
         return $stmt->execute([$usuario, $contrasenya]);
 
-     }
-
+    }
 
 
 
