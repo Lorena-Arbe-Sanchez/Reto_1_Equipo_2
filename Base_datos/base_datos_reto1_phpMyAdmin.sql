b9c5f5bbb8fc9897f91e0db512a415ee653d748b
Lorena-Arbe-Sanchez <126677607+Lorena-Arbe-Sanchez@users.noreply.github.com>
2024-11-02 18:19:41 +0100

    Update base_datos_reto1_phpMyAdmin.sql

--- Base_datos/base_datos_reto1_phpMyAdmin.sql
+++ Base_datos/base_datos_reto1_phpMyAdmin.sql
@@ -1,152 +1,69 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: mysql:3306
--- Tiempo de generación: 24-10-2024 a las 08:33:24
--- Versión del servidor: 11.5.2-MariaDB-ubu2404
--- Versión de PHP: 8.2.24
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         8.0.30 - MySQL Community Server - GPL
+-- SO del servidor:              Win64
+-- phpMyAdmin Adaptado
+-- --------------------------------------------------------
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Desactivar temporalmente las comprobaciones de claves foráneas y otros ajustes.
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+SET NAMES utf8mb4;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `grupo2_2425`
---
-CREATE DATABASE IF NOT EXISTS `grupo2_2425` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci;
+-- Crear la base de datos y seleccionarla.
+CREATE DATABASE IF NOT EXISTS `grupo2_2425` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `grupo2_2425`;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `preguntas`
---
-
-DROP TABLE IF EXISTS `preguntas`;
-CREATE TABLE `preguntas` (
-  `id` int(10) NOT NULL,
-  `titulo` varchar(100) NOT NULL,
-  `descripcion` varchar(100) NOT NULL,
-  `tema` varchar(100) NOT NULL,
+-- Crear la tabla `preguntas`
+CREATE TABLE IF NOT EXISTS `preguntas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tema` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `fecha` date NOT NULL,
-  `id_usuario` int(4) NOT NULL,
-  `archivo` varchar(255) DEFAULT NULL,
-  `votosLike` int(3) DEFAULT NULL,
-  `votosDislike` int(3) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+  `id_usuario` int NOT NULL,
+  `archivo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `votosLike` int DEFAULT NULL,
+  `votosDislike` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `PREG_ID_USU_FK` (`id_usuario`),
+  CONSTRAINT `PREG_ID_USU_FK` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+-- Crear la tabla `respuestas`
+CREATE TABLE IF NOT EXISTS `respuestas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `solucion` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `archivo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `votosLike` int DEFAULT NULL,
+  `votosDislike` int DEFAULT NULL,
+  `id_pregunta` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `RESP_ID_PREG_FK` (`id_pregunta`),
+  KEY `RESP_ID_USU_FK` (`id_usuario`),
+  CONSTRAINT `RESP_ID_PREG_FK` FOREIGN KEY (`id_pregunta`) REFERENCES `preguntas` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `RESP_ID_USU_FK` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Estructura de tabla para la tabla `respuestas`
---
-
-DROP TABLE IF EXISTS `respuestas`;
-CREATE TABLE `respuestas` (
-  `id` int(100) NOT NULL,
-  `solucion` varchar(200) NOT NULL,
-  `archivo` varchar(255) DEFAULT NULL,
-  `votosLike` int(3) DEFAULT NULL,
-  `votosDislike` int(3) DEFAULT NULL,
-  `id_pregunta` int(10) NOT NULL,
-  `id_usuario` int(4) NOT NULL,
-  `fecha` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-DROP TABLE IF EXISTS `usuarios`;
-CREATE TABLE `usuarios` (
-  `id` int(4) NOT NULL,
+-- Crear la tabla `usuarios`
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `administrador` tinyint(1) NOT NULL,
-  `dni` varchar(9) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `apellido1` varchar(50) NOT NULL,
-  `apellido2` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `telefono` varchar(9) DEFAULT NULL,
-  `usuario` varchar(50) NOT NULL,
-  `contrasena` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+  `dni` varchar(9) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido1` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido2` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(9) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `usuario` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contrasena` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `imagen` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `dni` (`dni`),
+  UNIQUE KEY `usuario` (`usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `preguntas`
---
-ALTER TABLE `preguntas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `PREG_ID_USU_FK` (`id_usuario`);
-
---
--- Indices de la tabla `respuestas`
---
-ALTER TABLE `respuestas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `RESP_ID_PREG_FK` (`id_pregunta`),
-  ADD KEY `RESP_ID_USU_FK` (`id_usuario`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `dni` (`dni`),
-  ADD UNIQUE KEY `usuario` (`usuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `preguntas`
---
-ALTER TABLE `preguntas`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `respuestas`
---
-ALTER TABLE `respuestas`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `preguntas`
---
-ALTER TABLE `preguntas`
-  ADD CONSTRAINT `PREG_ID_USU_FK` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
-
---
--- Filtros para la tabla `respuestas`
---
-ALTER TABLE `respuestas`
-  ADD CONSTRAINT `RESP_ID_PREG_FK` FOREIGN KEY (`id_pregunta`) REFERENCES `preguntas` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `RESP_ID_USU_FK` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Restaurar las comprobaciones de claves foráneas y otros ajustes.
+SET FOREIGN_KEY_CHECKS=1;
