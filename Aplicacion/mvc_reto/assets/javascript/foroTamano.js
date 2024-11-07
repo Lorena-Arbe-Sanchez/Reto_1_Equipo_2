55488d729fe22615b8a7b3ac71961c9209a5fb8d
JulenCorpas2004 <julen.corpas@ikasle.egibide.org>
2024-11-07 11:52:37 +0100

    tamaño del foro mejorado

--- Aplicacion/mvc_reto/assets/javascript/foroTamano.js
+++ Aplicacion/mvc_reto/assets/javascript/foroTamano.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const preguntas = document.querySelectorAll(".pag_foro .pregunta"); // Selecciona todas las preguntas
     const questionCount = preguntas.length; // Cuenta el número de preguntas
 
+    const cuadrosRespuestas = document.querySelectorAll(".pag_foro .pregunta .respuestas");
+
     // Actualiza la variable CSS con el nuevo conteo
     document.documentElement.style.setProperty('--question-count', questionCount);
 
@@ -13,4 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
         document.documentElement.style.setProperty('--base-margin', '450px'); // Para 3 o más preguntas
     }
+
+    let alturaCuadroRespuestas = 175;
+
+    cuadrosRespuestas.forEach(cuadroRespuestas => {
+        const respuestas = cuadroRespuestas.querySelectorAll(".respuesta");
+        const respuestaCount = respuestas.length;
+
+        cuadroRespuestas.style.setProperty('--altura-respuestas', alturaCuadroRespuestas * respuestaCount + 'px');
+        
+    });
 });
